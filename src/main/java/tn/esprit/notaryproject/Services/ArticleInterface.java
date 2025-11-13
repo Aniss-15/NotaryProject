915d3c942034cca5e1cf33bba9fd0a2d81f59143
Anis Chebbi <80129603+Aniss-15@users.noreply.github.com>
2025-11-13 14:50:38 +0100

    validation

--- src/main/java/tn/esprit/notaryproject/Services/ArticleInterface.java
+++ src/main/java/tn/esprit/notaryproject/Services/ArticleInterface.java
@@ -7,7 +7,7 @@ import java.sql.Date;
 import java.util.List;
 
 public interface ArticleInterface {
-    //lakrimm
+    //validation
     Article addArticle(Article article);
     Article updateArticle(Article article);
     void deleteArticle(Long IdArticle);
