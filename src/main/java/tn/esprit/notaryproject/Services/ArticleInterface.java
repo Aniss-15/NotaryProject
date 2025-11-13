063b3f7a2e30c98d698acdb4ff8db835fdd8c3b2
Anis Chebbi <80129603+Aniss-15@users.noreply.github.com>
2025-11-13 12:59:21 +0100

    commen1t

--- src/main/java/tn/esprit/notaryproject/Services/ArticleInterface.java
+++ src/main/java/tn/esprit/notaryproject/Services/ArticleInterface.java
@@ -7,6 +7,7 @@ import java.sql.Date;
 import java.util.List;
 
 public interface ArticleInterface {
+    //hello
     Article addArticle(Article article);
     Article updateArticle(Article article);
     void deleteArticle(Long IdArticle);
