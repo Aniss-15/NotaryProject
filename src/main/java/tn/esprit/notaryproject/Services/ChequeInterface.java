dc0f777e6186cd725a123b97f9b01fa70b1f6189
Anis Chebbi <80129603+Aniss-15@users.noreply.github.com>
2025-11-16 15:54:49 +0100

    testfinal

--- src/main/java/tn/esprit/notaryproject/Services/ChequeInterface.java
+++ src/main/java/tn/esprit/notaryproject/Services/ChequeInterface.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.util.List;
 
 public interface ChequeInterface {
-    // OCR text extraction methode
+    // OCR text extraction methodes
      ChequeValidationStatus checkChequeValidity(MultipartFile file) throws Exception ;
      Cheque chequeAjout(Cheque cheque, MultipartFile file) throws Exception ;
      void DeleteCheque(Long IdCheque) ;
